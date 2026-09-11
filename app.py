9cef748ced5142a87284ced2565c4d145fa998b3
Ankush Sharma <139066519+ankushsharma30@users.noreply.github.com>
2026-09-12 01:45:53 +0530

    Refactor Streamlit app for customer churn prediction

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ import tensorflow as tf
 from sklearn.preprocessing import StandardScaler, OneHotEncoder,LabelEncoder
 import pickle
 
-model = tf.keras.models.load_model('ANN project/model.h5')
+model = tf.keras.models.load_model('model.h5')
 
-with open('ANN project/onehot_encoder_geo.pkl','rb') as file:
+with open('onehot_encoder_geo.pkl','rb') as file:
     ohe_geo = pickle.load(file)
     
-with open('ANN project/label_encoder_gender.pkl','rb') as file:
+with open('label_encoder_gender.pkl','rb') as file:
     label_encoder_gender = pickle.load(file)
 
-with open('ANN project/scaler.pkl','rb') as file:
+with open('scaler.pkl','rb') as file:
     scaler = pickle.load(file)
 
 ## streamlit app
